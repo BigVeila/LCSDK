c9b227eaa62ed1a5294e1d77622054cfaef7babd
David Bui <haibuihoang@vccorp.vn>
2020-06-19 09:27:04 +0700

    Creating and publishing Universal Pod

---
+++ LifeCardSDK.framework/Headers/LifeCardSDK.h
@@ -0,0 +1,20 @@
+//
+//  LifeCardSDK.h
+//  LifeCardSDK
+//
+//  Created by BigSun on 6/17/20.
+//  Copyright © 2020 BigSun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import "MLTransition.h"
+//#import "UITableViewCell+CellShadows.h"
+//! Project version number for LifeCardSDK.
+FOUNDATION_EXPORT double LifeCardSDKVersionNumber;
+
+//! Project version string for LifeCardSDK.
+FOUNDATION_EXPORT const unsigned char LifeCardSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifeCardSDK/PublicHeader.h>
+
+
